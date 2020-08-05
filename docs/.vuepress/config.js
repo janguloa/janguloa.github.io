5c0538442cceeb4c306c8900661ba78c35ceb657
Jesus <jsuesalnugo@gmail.com>
2020-08-05 13:15:18 -0500

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    base: "/",
+    title: 'Pana Upgrade 2',
+    description: 'Tiene que servir todo',
+    themeConfig: {
+        nav: [
+          { text: 'inicio', link: '/' },
+          { text: 'Guía', link: '/guia/' },
+          { text: 'Canal youtube', link: 'https://www.youtube.com/watch?v=zk6BwgxnBtE' }
+        ],
+        sidebar:{
+          '/guia/':[
+            ''
+          ]
+        }
+      }
+  }
